5a562bb15238958a3b1f5b2a6818bf3b91e38a74
Sergei Iakupov <jakupov@gmail.com>
2020-09-18 04:18:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleString.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string Text{ "jrioewjmrt" };
+
+    std::cout << "Text = " << Text << "\n";
+    std::cout << "Length = " << Text.length() << "\n";
+    std::cout << "First symbol = " << Text[0] << "\n";
+    std::cout << "Last symbol = " << Text[Text.length() - 1] << "\n";
+}
